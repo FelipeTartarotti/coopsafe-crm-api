3b53236442b89774dab6943541236e7e37e261c9
FelipeTartarotti <B@io2490tarta>
2020-10-01 09:42:53 -0300

    crud

--- crm-api/quotation/models.py
+++ crm-api/quotation/models.py
@@ -20,11 +20,12 @@ class VehicleSpecie(models.Model):
 
 class PlanPrices(models.Model):
 
-    vehicle_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    vehicle_price_from = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    vehicle_price_to = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     plan_price = models.DecimalField(max_digits=10, decimal_places=2 , null=True, blank=True)
 
     def __str__(self):
-        return str(self.vehicle_price)
+        return str(self.vehicle_price_from)
 
 
 class Plan(models.Model):
