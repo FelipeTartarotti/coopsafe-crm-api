28c890336a8d7d96325c50f86f44b80c795d59a9
FelipeTartarotti <B@io2490tarta>
2020-10-01 10:34:55 -0300

    admin

--- crm-api/request_log/admin.py
+++ crm-api/request_log/admin.py
@@ -4,7 +4,7 @@ from request_log import models
 
 class RequestLogAdmin(admin.ModelAdmin):
     list_display = ('endpoint','status_code',
-                    'method','ip', 'user',
+                    'method','ip','user',
                     'time', 'response_time')
 
 admin.site.register(models.RequestLog,RequestLogAdmin)
